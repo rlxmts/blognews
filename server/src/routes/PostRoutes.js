f4c5d738522332953894a9a201432d6f111fd53e
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-08-19 16:36:14 -0300

    :recycle: Removendo linha vazia

--- server/src/routes/PostRoutes.js
+++ server/src/routes/PostRoutes.js
@@ -1,6 +1,5 @@
 import express from "express";
 import PostController from "../controllers/postControllers.js";
-
 const routes = express.Router();
 routes.get("/posts", PostController.listarPosts);
 export default routes;
