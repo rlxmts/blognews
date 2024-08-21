6c8a2279c27ebf9789ab8a16604eff3232f367d8
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-08-21 13:08:07 -0300

    :sparkles: Incluindo galeria de post

--- client/src/Pages/Painel/index.jsx
+++ client/src/Pages/Painel/index.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import GaleriaPosts from "../../components/Sections/GaleriaPosts";
 
 const Painel = () => {
     return(
         <section>
             <h1>Painel Administrativo</h1>
             <Link to="/novo-post">Criar novo Post</Link>
+            <GaleriaPosts />
         </section>
     )
 }
