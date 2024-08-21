329fff3ab9fcb8b54502c1dcac49c866307247cb
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-08-21 13:09:18 -0300

    :sparkles: Incluindo rota para método POST

--- server/src/routes/PostRoutes.js
+++ server/src/routes/PostRoutes.js
@@ -2,4 +2,5 @@ import express from "express";
 import PostController from "../controllers/postControllers.js";
 const routes = express.Router();
 routes.get("/posts", PostController.listarPosts);
+routes.post("/posts", PostController.cadastrarPost);
 export default routes;
