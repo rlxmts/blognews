b323c92d9c5c411cf34622d0fccd356ef25f2c94
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-08-21 12:05:48 -0300

    :sparkles: Criando pagina home/ alterando rota para home

---
+++ client/src/Pages/Home/index.jsx
@@ -0,0 +1,8 @@
+import GaleriaPosts from "../../components/Sections/GaleriaPosts"
+
+const Home = ()=> {
+    return(
+        <GaleriaPosts />
+    )
+}
+export default Home;
--- client/src/Pages/Rotas.jsx
+++ client/src/Pages/Rotas.jsx
@@ -1,17 +1,17 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Cabecalho from "../components/Layout/Cabecalho";
-import GaleriaPosts from "../components/Sections/GaleriaPosts";
 import Rodape from "../components/Layout/Rodape";
 import Login from "./Login";
 import Painel from "./Painel";
 import NovoPost from "./NovoPost";
+import Home from "./Home";
 
 const AppRotas = () => {
     return(
         <BrowserRouter>
         <Cabecalho />
         <Routes>
-            <Route path="/" element={<GaleriaPosts />} />
+            <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/admin" element={<Painel />} />
             <Route path="/novo-post" element={<NovoPost />} />
