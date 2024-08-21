587429285bf8646ee4919bf3a3278e4521c09ec1
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-08-21 16:56:16 -0300

    :sparkles: Incluindo rota de Buscar Posts

--- server/src/routes/PostRoutes.js
+++ server/src/routes/PostRoutes.js
@@ -2,5 +2,6 @@ import express from "express";
 import PostController from "../controllers/postControllers.js";
 const routes = express.Router();
 routes.get("/posts", PostController.listarPosts);
+routes.get("/posts/:id", PostController.buscarPost);
 routes.post("/posts", PostController.cadastrarPost);
 export default routes;
