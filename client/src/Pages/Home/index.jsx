a44b383d59629665f539279dbf25c62a7ebd2a8b
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-08-21 13:06:57 -0300

    :sparkles: Incluindo titulo

--- client/src/Pages/Home/index.jsx
+++ client/src/Pages/Home/index.jsx
@@ -1,8 +1,21 @@
+import styled from "styled-components";
 import GaleriaPosts from "../../components/Sections/GaleriaPosts"
+
+const Section = styled.section`
+    padding: 5rem 0;
+`
+
+const H2 = styled.h2`
+    font-weight: 500;
+    text-align: center;
+`
 
 const Home = ()=> {
     return(
-        <GaleriaPosts />
+        <Section>            
+            <H2> Posts recentes:</H2>
+            <GaleriaPosts />
+        </Section>
     )
 }
 export default Home;
