a958aafbc2dbdd15700e438775c96fc7ed3c4209
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-08-21 11:24:50 -0300

    :sparkles: Adicionando rotas: Novo post, painel

--- client/src/Pages/Rotas.jsx
+++ client/src/Pages/Rotas.jsx
@@ -3,6 +3,8 @@ import Cabecalho from "../components/Layout/Cabecalho";
 import GaleriaPosts from "../components/Sections/GaleriaPosts";
 import Rodape from "../components/Layout/Rodape";
 import Login from "./Login";
+import Painel from "./Painel";
+import NovoPost from "./NovoPost";
 
 const AppRotas = () => {
     return(
@@ -11,6 +13,8 @@ const AppRotas = () => {
         <Routes>
             <Route path="/" element={<GaleriaPosts />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/admin" element={<Painel />} />
+            <Route path="/novo-post" element={<NovoPost />} />
         </Routes>
         <Rodape />
         </BrowserRouter>
